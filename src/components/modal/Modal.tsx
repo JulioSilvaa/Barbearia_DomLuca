56ec5e77c0d72fca8c820ef7c8b368e46c6d23b7
JulioSilvaa <juliocesar.amancio@yahoo.com.br>
2024-05-17 13:32:42 -0300

    style: formatando form no modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,4 +1,5 @@
 import { useRef, useEffect } from 'react';
+import Form from '../form/Form';
 
 const Modal = ({ stateModal }: any) => {
   const modalRef = useRef<HTMLDivElement>(null);
@@ -18,20 +19,16 @@ const Modal = ({ stateModal }: any) => {
   }, [stateModal]);
 
   return (
-    <div ref={modalRef} className="absolute flex flex-col gap-4 text-center top-1 left-0 bg-zinc-800 right-0 z-50 opacity-90">
+    <div ref={modalRef} className="absolute flex flex-col gap-4 text-center top-1 left-0 bottom-80 bg-zinc-800 right-0 z-50 opacity-90">
       <div
         onClick={() => stateModal(false)}
-        className="text-right lg:block text-[3rem] mt-2 mr-10 cursor-pointer p-2"
+        className="text-right lg:block text-[2rem] mt-2 mr-8 cursor-pointer p-1"
       >
         <p>X</p>
       </div>
-      <div className='py-6'>
-      <h1 className="text-[3rem]">AQUI VAI O FORMEPARA AGENDAMENTO</h1>
-      <div className='text-[2rem]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi libero sint quia fuga quidem quis dolorem? Accusantium doloribus molestias deserunt?</div>
-      <div className='text-[2rem]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi libero sint quia fuga quidem quis dolorem? Accusantium doloribus molestias deserunt?</div>
-      <div className='text-[2rem]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi libero sint quia fuga quidem quis dolorem? Accusantium doloribus molestias deserunt?</div>
-      <div className='text-[2rem]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi libero sint quia fuga quidem quis dolorem? Accusantium doloribus molestias deserunt?</div>
-      <div className='text-[2rem]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi libero sint quia fuga quidem quis dolorem? Accusantium doloribus molestias deserunt?</div>
+      <div className='py-2'>
+      <h1 className="text-[3rem]">Cadastro</h1>
+      <Form/>
       </div>
     </div>
   );
